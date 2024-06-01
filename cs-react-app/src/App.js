ca7302f59602285ea5b7f029b9d92ffc51ff14de
Deanna R. Scott <deereneescott@gmail.com>
2024-06-01 09:24:20 -0400

    classwork from 5-28

--- cs-react-app/src/App.js
+++ cs-react-app/src/App.js
@@ -1,63 +1,31 @@
-import users from "./users.json";
-import todos from "./todos.json";
-console.log(users);
-console.log(todos);
+import react, {useState} from "react";
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
+import {tasks} from "./data";
 
-const greeting = "Welcome to React";
-const user = "Cohort 2024";
+import { greeting, user } from "./data";
 
 function App() {
+  const [myTasks, setMyTasks] = useState(tasks);
+
+  const handleFormSubmit = (e)=> {
+    e.preventDefault();
+    const newTask = {
+      id: myTasks.length = 1,
+      title: e.target.taskName.value,
+    }
+  }
+
   return (
     <div className="App">
       <h1>
         {greeting} {user}
       </h1>
 
-      <Users />
-      <Todos />
+      <TaskForm />
+      <TaskList tasks={myTasks}/>
     </div>
   );
-}
-
-function Users() {
-  return (
-    <div>
-      <h2>Users</h2>
-      
-      {users.map((user) => (
-        <div>
-          <h3>{user.name}</h3>
-          <p>Email:</p>
-          <a href={`mailto:${user.email}`}>{user.email}</a>
-          <p>Address: </p>
-          <address>
-            <p>
-              {user.address.street} {user.address.suite}
-            </p>
-            <p>
-              {user.address.city} {user.address.zipcode}
-            </p>
-          </address>
-          <p>Company:</p>
-          <p>{user.company.name}</p>
-          <p>{user.company.catchPhrase}</p>
-        </div>
-      ))}
-    </div>
-  )
-  
-};
-
-function Todos() {
-  return (<div>
-    <h2>To Dos</h2>
-    {todos.map((todo)=> (
-      <div>
-        <p>To Do: {todo.title}</p>
-        <p>Status: {todo.completed ? "Completed" : "In progress"}</p>
-      </div>
-    ))}
-  </div>);
 }
 
 export default App;
---
+++ cs-react-app/src/components/TaskList.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+
+function TaskList(props) {
+  return (
+    <div>
+      {props.tasks.map((task) => (
+        <ul>
+          <li>Task: {task.title}</li>
+          <li>Status: {task.status}</li>
+        </ul>
+      ))}
+    </div>
+  );
+}
+
+export default TaskList;
